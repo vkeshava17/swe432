e63e32df2b6a4f3c5260a6f2421dbad27c84e888
Vandana Keshavamurthy <vandanakeshavamurthy@Vandanas-MacBook-Pro.local>
2020-04-26 20:02:25 -0400

    removed extra

--- src/main/java/servlet/assignment8.java
+++ src/main/java/servlet/assignment8.java
@@ -354,7 +354,7 @@ public class assignment8 extends HttpServlet
     }
 
     int mostQ1 = 5;
-    String lifeCycleURL = "swe432vkeshava.herokuapp.com/asst8part2";
+    String lifeCycleURL = "asst8part2";
     out.println("<a href=\"" + lifeCycleURL + "?commonQ1=mostQ1\">");
     out.println("Aggregate Summary</a>");
 
